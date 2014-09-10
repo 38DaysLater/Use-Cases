1e4ac921259487c0a66443edb4038881b9ad90c3
olahay3 <shirotsubame@gmail.com>
2014-09-10 00:24:53 -0400

    Olivia updated Controller class for loading screen

--- SpaceTrader/src/spacetrader/FXMLDocumentController.java
+++ SpaceTrader/src/spacetrader/FXMLDocumentController.java
@@ -11,26 +11,48 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 /**
+ * FXML Controller class
  *
- * @author Sarah
+ * @author Olivia
  */
 public class FXMLDocumentController implements Initializable {
-    
+    @FXML
+    private AnchorPane loadingPane;
+    @FXML
+    private ImageView imageView;
     @FXML
     private Label label;
-    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private Button loadGameButton;
+    @FXML
+    private Button newGameButton;
+    @FXML
+    private Button optionsButton;
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void handleLoadButtonAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleNewGameButtonAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleOptionButtonAction(ActionEvent event) {
+    }
     
 }
