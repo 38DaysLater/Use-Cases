7c92423681c851ea9e2f0ea2079a27ab48e4302d
Sarah Alsmiller <ramvling@gmail.com>
2014-09-12 13:38:54 -0400

    Simple dialog test

--- SpaceTrader/src/spacetrader/FXMLDocumentController.java
+++ SpaceTrader/src/spacetrader/FXMLDocumentController.java
@@ -6,6 +6,12 @@
 
 package spacetrader;
 
+import static org.controlsfx.dialog.Dialog.Actions.NO;
+import static org.controlsfx.dialog.Dialog.Actions.YES;
+import impl.org.controlsfx.i18n.Localization;
+import impl.org.controlsfx.i18n.Translations;
+import org.controlsfx.control.action.Action;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -15,6 +21,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import org.controlsfx.dialog.Dialogs;
 
 /**
  * FXML Controller class
@@ -43,12 +50,24 @@ public class FXMLDocumentController implements Initializable {
         // TODO
     }    
 
-    @FXML
-    private void handleLoadButtonAction(ActionEvent event) {
-    }
 
     @FXML
     private void handleNewGameButtonAction(ActionEvent event) {
+        //launches a simple dialouge box and collects user input. Just testing out
+        //the controlsFX API
+        
+        //as far as I can tell we should be able to make the dialog box contents
+        //in a seperate file, create it as a new content object, and launch it that way
+        //api here: http://controlsfx.bitbucket.org/org/controlsfx/dialog/Dialog.html
+        Action response = Dialogs.create()
+                .title("You do want dialogs right?")
+                .masthead("Just Checkin'")
+                .message( "I was a bit worried that you might not want them, so I wanted to double check.")
+                .showConfirm();
+    }
+    
+        @FXML
+    private void handleLoadButtonAction(ActionEvent event) {
     }
 
     @FXML
