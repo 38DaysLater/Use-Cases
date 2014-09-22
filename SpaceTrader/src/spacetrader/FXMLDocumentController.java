807e9e1c75745e62c474bc5309d0cb0c9b2009c3
Lawrence Moore <lsmoore938@gmail.com>
2014-09-22 19:55:56 -0400

    first commit

--- SpaceTrader/src/spacetrader/FXMLDocumentController.java
+++ SpaceTrader/src/spacetrader/FXMLDocumentController.java
@@ -6,22 +6,31 @@
 
 package spacetrader;
 
-import static org.controlsfx.dialog.Dialog.Actions.NO;
-import static org.controlsfx.dialog.Dialog.Actions.YES;
 import impl.org.controlsfx.i18n.Localization;
 import impl.org.controlsfx.i18n.Translations;
-import org.controlsfx.control.action.Action;
-
+import java.awt.Dialog;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import org.controlsfx.control.action.Action;
+import static org.controlsfx.dialog.Dialog.Actions.NO;
+import static org.controlsfx.dialog.Dialog.Actions.YES;
 import org.controlsfx.dialog.Dialogs;
+//import org.controlsfx.dialog.Dialog.Actions;
+//import org.controlsfx.dialog.Dialog;
+
 
 /**
  * FXML Controller class
@@ -41,6 +50,9 @@ public class FXMLDocumentController implements Initializable {
     private Button newGameButton;
     @FXML
     private Button optionsButton;
+    @FXML
+    private FXMLLoader loader = new FXMLLoader();
+   
 
     /**
      * Initializes the controller class.
@@ -60,18 +72,41 @@ public class FXMLDocumentController implements Initializable {
         //in a seperate file, create it as a new content object, and launch it that way
         //api here: http://controlsfx.bitbucket.org/org/controlsfx/dialog/Dialog.html
         Action response = Dialogs.create()
-                .title("You do want dialogs right?")
-                .masthead("Just Checkin'")
-                .message( "I was a bit worried that you might not want them, so I wanted to double check.")
+                .title("New Game")
+                .masthead("Confirm new game?")
+                //.message( "")
+                //.actions(Dialog.Actions.OK, Dialog.Actions.CANCEL)
                 .showConfirm();
+        // Should direct to a new screen upon hitting "yes" but it doesn't work. Anyone have ideas?
+        if (response == Dialog.Actions.YES) {
+            URL url = getClass().getResource("CharacterCreation.fxml");
+            loader.setLocation(url);
+            loader.setBuilderFactory(new JavaFXBuilderFactory());
+            try {
+             loadingPane = (AnchorPane) loader.load(url.openStream());
+             } catch (IOException ex) {
+              Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            
+        }
+        //loadingPane.getChildren().setAll(FXMLLoader.load(filename));
+     
     }
     
         @FXML
     private void handleLoadButtonAction(ActionEvent event) {
+        Action response = Dialogs.create()
+                .title("Load Game")
+                .masthead("Load last saved game?")
+                .showConfirm();
     }
 
     @FXML
     private void handleOptionButtonAction(ActionEvent event) {
+        Action response = Dialogs.create()
+                .title("Options")
+                .showConfirm();
     }
     
 }
